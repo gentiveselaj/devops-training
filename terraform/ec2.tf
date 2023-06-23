8031b46cc60f46b96c8e49e02b0de2b6e202fa13
gentiveselaj <gentzveselaj@gmail.comm>
2023-06-23 20:03:14 +0200

    error ec2

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -3,13 +3,13 @@ resource "aws_network_interface" "web" {
   security_groups = ["sg-0dd3a367880737f7a"]
 }
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
+  ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
  network_interface {
     network_interface_id = aws_network_interface.web.id
     device_index         = 0
   }
   tags = {
-    Name = "appstellar-${var.enviroment}"
+    Name = "appstellar-gennntt"
   }
 }
