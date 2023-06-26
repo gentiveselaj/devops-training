0f3f934f0f2752964a231c8fdf61ffef211f2409
gentiveselaj <gentzveselaj@gmail.comm>
2023-06-26 21:05:50 +0200

    terraform-aws-lab

---
+++ terraform-modules-lab/ec2.tf
@@ -0,0 +1,4 @@
+module "ec2" {
+    source = "app.terraform.io/appstellar-devops-training/ec/aws"
+    version = "1.0.0"
+}
--- terraform/modules/ec2-instance/main.tf
+++ terraform/modules/ec2-instance/main.tf
@@ -1,16 +1,16 @@
 resource "aws_network_interface" "web" {
-  subnet_id       = "subnet-03ddf0d4fd589ae9a"
-  security_groups = ["sg-0dd3a367880737f7a"]
+  subnet_id       = var.subnet_id
+  security_groups = var.security_group
 }
 
 resource "aws_instance" "web" {
   ami           = "${data.aws_ami.ubuntu.id}"
-  instance_type = "t2.micro"
+  instance_type = var.type
  network_interface {
     network_interface_id = aws_network_interface.web.id
-    device_index         = 0
+    device_index         = var.device_index
   }
   tags = {
-    Name = "appstellar-gennntt"
+    Name = "${var.instance_name}-${var.enviroment}"
   }
 }
